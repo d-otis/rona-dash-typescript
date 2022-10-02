2c000a3d9513e531b05054cf0ed914b6d276413b
Dan Foley <dan@danfoley.dev>
2022-10-02 15:28:29 -0400

    add Navigation

---
+++ src/components/Navigation/index.test.tsx
@@ -0,0 +1,31 @@
+import { render, screen } from '@testing-library/react';
+import Navigation from './index';
+
+const setup = () => {
+  render(<Navigation />);
+};
+
+describe('Navigation', () => {
+  it('loads with correct title', () => {
+    setup();
+    const title = screen.getByText(/rona dash/i);
+    expect(title).toBeInTheDocument();
+  });
+
+  it('includes all the require menu items', () => {
+    setup();
+    const globalLink = screen.getByText(/global/i);
+    const continents = screen.getByText(/continents/i);
+    const countries = screen.getByText(/countries/i);
+    const usStates = screen.getByText('US States');
+    const about = screen.getByText(/about/i);
+    const refresh = screen.getByText(/refresh/i);
+
+    expect(globalLink).toBeInTheDocument();
+    expect(continents).toBeInTheDocument();
+    expect(countries).toBeInTheDocument();
+    expect(usStates).toBeInTheDocument();
+    expect(about).toBeInTheDocument();
+    expect(refresh).toBeInTheDocument();
+  });
+});
