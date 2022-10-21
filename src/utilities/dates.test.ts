362da548a644240b3f0f7ccbae2f4eaee43db438
Dan Foley <dan@danfoley.dev>
2022-10-20 22:58:50 -0400

    define rewindDate()

--- src/utilities/dates.test.ts
+++ src/utilities/dates.test.ts
@@ -1,4 +1,4 @@
-import { currentDate } from './dates';
+import { currentDate, rewindDate } from './dates';
 
 describe('currentDate()', () => {
   it('returns the date in the correct format', () => {
@@ -9,5 +9,14 @@ describe('currentDate()', () => {
       .split(',')[0];
 
     expect(currentDate()).toBe(today);
+  });
+});
+
+describe('rewindDate()', () => {
+  it("returns yesterday's date in the correct format", () => {
+    const yesterday = '10-19-2022';
+    const today = '10-20-2022';
+
+    expect(rewindDate(today)).toBe(yesterday);
   });
 });
--- src/utilities/dates.ts
+++ src/utilities/dates.ts
@@ -1,3 +1,12 @@
+import dayjs from 'dayjs';
+import customParseFormat from 'dayjs/plugin/customParseFormat';
+dayjs.extend(customParseFormat);
+
 export const currentDate = (): string => {
   return new Date().toLocaleString('en-US').replaceAll('/', '-').split(',')[0];
+};
+
+export const rewindDate = (date: string): string => {
+  const oneDayBack = dayjs(date, 'MM-DD-YYYY').subtract(1, 'day');
+  return dayjs(oneDayBack).format('MM-DD-YYYY');
 };
